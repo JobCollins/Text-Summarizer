dfa01b88a0560533da8e592b0213352e71aff9b8
JobCollins <dulo.collins@gmail.com>
2024-03-12 22:07:04 +0300

    Bug: 'NoneType' object has no attribute 'split'
    transformer trainer.train returns None

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from textSummarizer.logging import logger
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from textSummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from textSummarizer.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -24,7 +25,7 @@ except Exception as e:
         logger.exception(e)
         raise e
 
-STAGE_NAMETAGE_NAME = "Data Transformation stage"
+STAGE_NAME = "Data Transformation stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_transformation = DataTransformationTrainingPipeline()
@@ -34,3 +35,13 @@ except Exception as e:
         logger.exception(e)
         raise e
 
+STAGE_NAME = "Model Trainer stage"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainerTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
